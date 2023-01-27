56b38a5efdcd380b0346f3ea919c691db4135900
rhonniel <rhonniel777@gmail.com>
2023-01-26 23:15:42 -0400

    Ajustando entidad para cumplir con estandar de relacional actual. Las relaciones de de uno a mucho no seran expresadas con listas por ahora

--- src/com/mercado/model/product/Recipe.java
+++ src/com/mercado/model/product/Recipe.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Recipe {
     private int id;
     private String name;
-    private List<StepRecipe> stepsRecipe;
     private boolean isActive;
 
     public int getId() {
@@ -22,14 +21,6 @@ public class Recipe {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public List<StepRecipe> getStepsRecipe() {
-        return stepsRecipe;
-    }
-
-    public void setStepsRecipe(List<StepRecipe> stepsRecipe) {
-        this.stepsRecipe = stepsRecipe;
     }
 
     public boolean isActive() {
