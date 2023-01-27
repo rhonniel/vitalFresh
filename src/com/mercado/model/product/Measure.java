981085b1adce3d59056a7983db797a212af3ee32
rhonniel <rhonniel777@gmail.com>
2023-01-27 15:37:09 -0400

    feature(product): creation of entity Measure
    Measures are kind of unit for ingredients (liter,grams). These can have many types , for example: volume,weight.

---
+++ src/com/mercado/model/product/Measure.java
@@ -0,0 +1,35 @@
+package com.mercado.model.product;
+
+import com.mercado.model.BaseEntity;
+
+public class Measure extends BaseEntity {
+    private String name;
+    private String abbreviation;
+    private MeasureType measureType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public MeasureType getMeasureType() {
+        return measureType;
+    }
+
+    public void setMeasureType(MeasureType measureType) {
+        this.measureType = measureType;
+    }
+
+
+}
---
+++ src/com/mercado/model/product/MeasureType.java
@@ -0,0 +1,16 @@
+package com.mercado.model.product;
+
+import com.mercado.model.BaseEntity;
+
+
+public class MeasureType extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
