625c875ab7b8a2fa4a3fcb74318b07ec428687bd
rhonniel <rhonniel777@gmail.com>
2023-01-28 16:41:35 -0400

    feature(product): creation of entity BaseEntity - BaseEntity is an abstract class for every entity in the project.

---
+++ src/com/mercado/model/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.mercado.model;
+
+import com.mercado.model.product.Status;
+
+import java.util.Objects;
+
+public abstract class BaseEntity {
+    private Integer id;
+    private Status status;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
