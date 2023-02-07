9ca0d827e492a61644dab93d3d6f3fa089e30411
rhonniel <rhonniel777@gmail.com>
2023-02-06 20:58:44 -0400

    feature(add new field to Ingredient): - add Measure - add calories

--- src/com/mercado/model/product/Ingredient.java
+++ src/com/mercado/model/product/Ingredient.java
@@ -4,6 +4,24 @@ import com.mercado.model.BaseEntity;
 
 public class Ingredient extends BaseEntity {
     private String name;
+    private Measure measure;
+    private Integer calories;
+
+    public Measure getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(Measure measure) {
+        this.measure = measure;
+    }
+
+    public Integer getCalories() {
+        return calories;
+    }
+
+    public void setCalories(Integer calories) {
+        this.calories = calories;
+    }
 
     public String getName() {
         return name;
